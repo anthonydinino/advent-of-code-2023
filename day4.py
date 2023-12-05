058fc09c0fcfcb76a00efe37fd622083df930440
anthonydinino <adinino1996@gmail.com>
2023-12-06 03:26:19 +1030

    day4 p2 complete

--- day4.py
+++ day4.py
@@ -1,27 +1,26 @@
 def main():
     f = open("input/day4.txt", "r")
-    scoreTotal = []
+    scratchcards = {}
+    runsCount = 0
     for l in [x.rstrip() for x in f]:
-        numbers = [x.strip() for x in l.split(":")[1].strip().split("|")]
+        game = [x.strip() for x in l.split(":")]
+        gameNum = int([x for x in game[0].split(" ") if x][1])
+        numbers = game[1].strip().split("|")
         winningNumbers = [int(n) for n in numbers[0].split(" ") if n]
         myNumbers = [int(n) for n in numbers[1].split(" ") if n]
-        matches = 0
-        for n in myNumbers:
-            for w in winningNumbers:
-                if n == w:
-                    matches += 1
-        scoreTotal.append(calculateScore(matches))
-    print(sum(scoreTotal))
 
-
-def calculateScore(matches):
-    if matches < 1:
-        return 0
-    else:
-        score = 1
-        for i in range(1, matches):
-            score *= 2
-        return score
+        # start playing
+        runs = 1 + scratchcards.get(gameNum, 0)
+        runsCount += runs
+        while runs > 0:
+            temp = int(gameNum)
+            for n in myNumbers:
+                for w in winningNumbers:
+                    if n == w:
+                        temp += 1
+                        scratchcards[temp] = scratchcards.get(temp, 0) + 1
+            runs -= 1
+    print(runsCount)
 
 
 if __name__ == "__main__":
