4734bff3e8ce55bc44b931a555b42c214e8e5b90
anthonydinino <adinino1996@gmail.com>
2023-12-05 22:34:26 +1030

    day2 p2 completed

--- day2.py
+++ day2.py
@@ -1,26 +1,24 @@
 def main():
     f = open("input/day2.txt", "r")
-    p = {"red": 12, "green": 13, "blue": 14}
     total = 0
     for l in [x.rstrip() for x in f]:
-        id = int(l.split(":")[0].split(" ")[1])
         game = list(map(lambda x: x.strip(), l.split(":")[1].split(";")))
-        if not isGamePossible(game, p):
-            continue
-        else:
-            total += id
+        product = 1
+        for x in list(calculateMinGame(game).values()):
+            product *= x
+        total += product
     print(total)
 
 
 # e.g game = ["3 green, 15 blue, 14 red", "3 green, 15 blue, 14 red"]
-# e.g possible = {"red": 12, "green": 13, "blue": 14}
-def isGamePossible(game: list, p: dict):
+def calculateMinGame(game: list):
+    thisGame = {"red": 0, "green": 0, "blue": 0}
     for handful in game:
         for pick in [c.strip().split(" ") for c in handful.split(",")]:
             number, color = int(pick[0]), pick[1]
-            if number > p[color]:
-                return False
-    return True
+            if number > thisGame[color]:
+                thisGame[color] = number
+    return thisGame
 
 
 if __name__ == "__main__":
