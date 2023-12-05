2f66aa9b70c87df14489aecf0a154516f8f6faf6
anthonydinino <adinino1996@gmail.com>
2023-12-05 20:53:00 +1030

    day1 complete

--- day1.py
+++ day1.py
@@ -1,10 +1,34 @@
 f = open("input/day1.txt", "r")
 
+
+def getNum(s, mode="first"):
+    numbers = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
+    i = 0
+    frontChar = -1 if mode == "first" else 0
+    while i < len(s):
+        x = s[0 : i + 1] if mode == "first" else s[i * -1 if i != 0 else -1 :]
+        if x[frontChar].isdigit():
+            return x[frontChar]
+        for k in numbers.keys():
+            if k in x:
+                return str(numbers[k])
+        i += 1
+
+
 total = 0
-for x in f:
-    test = list(filter(lambda t: t.isdigit(), x))
-    test = int(test[0] + test[-1])
-    total += test
+for x in map(lambda x: x.rstrip(), f):
+    first, last = (getNum(x), getNum(x, "last"))
+    total += int((first or last) + (last or first))
 
 print(total)
-
